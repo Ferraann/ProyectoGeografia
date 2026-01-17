8952e80d3a529fe4032a330545d59d71ca419eb7
Ferraann <ferransansaloni@gmail.com>
2026-01-17 18:52:26 +0100

    responsive perfecto hasta aqui
    queda retocar

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,27 @@ window.addEventListener('scroll', () => {
         btnVolver.classList.remove('visible');
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Seleccionamos el botón y el menú
+    const menuToggle = document.getElementById('mobile-menu');
+    const nav = document.querySelector('nav');
+    const links = document.querySelectorAll('.nav-list li a');
+
+    // Evento al hacer clic en el botón hamburguesa
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('active'); // Abre/Cierra el menú
+        menuToggle.classList.toggle('is-active'); // Anima el botón
+    });
+
+    // (Opcional) Cerrar el menú automáticamente al hacer clic en un enlace
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            if(nav.classList.contains('active')) {
+                nav.classList.remove('active');
+                menuToggle.classList.remove('is-active');
+            }
+        });
+    });
+});
